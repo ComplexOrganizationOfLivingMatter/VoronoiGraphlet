9f2478bd73880dab71b30db608b7ff0acc0c9e58
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-01 14:02:32 +0100

    refactoring names

--- voronoiGraphlets/src/lib/ByPedro/Calculate_neighbors_polygon_distribution.m
+++ voronoiGraphlets/src/lib/ByPedro/Calculate_neighbors_polygon_distribution.m
@@ -66,13 +66,13 @@ function Calculate_neighbors_polygon_distribution(currentPath)
                     image = imread(fullPathFile);
                     image = im2bw(image(:,:,1), 0.2);
                     if sum(image(:) == 255) > sum(image(:) == 0) || sum(image(:) == 1) > sum(image(:) == 0)
-                        Img_L = watershed(1 - image, 8);
+                        L_img = watershed(1 - image, 8);
                     else
                         image = image == 0;
-                        Img_L = watershed(1 - image, 8);
+                        L_img = watershed(1 - image, 8);
                     end
 
-                    areas = regionprops(Img_L, 'Area');
+                    areas = regionprops(L_img, 'Area');
                     areas = [areas.Area];
                     areasMean = mean(areas);
                     incorrectAreas = areas > areasMean*50;
@@ -83,8 +83,8 @@ function Calculate_neighbors_polygon_distribution(currentPath)
                         image(min(zerosXs):max(zerosXs), min(zerosYs)) = 0;
                         image(max(zerosXs), min(zerosYs):max(zerosYs)) = 0;
                         image(min(zerosXs):max(zerosXs), max(zerosYs)) = 0;
-                        Img_L = watershed(1 - image, 8);
-                        areas = regionprops(Img_L, 'Area');
+                        L_img = watershed(1 - image, 8);
+                        areas = regionprops(L_img, 'Area');
                         areas = [areas.Area];
                         areasMean = mean(areas);
                         incorrectAreas = areas > areasMean*50;
@@ -97,16 +97,16 @@ function Calculate_neighbors_polygon_distribution(currentPath)
 
                     ratio=4;
                     se = strel('disk',ratio);
-                    cells=sort(unique(Img_L));
+                    cells=sort(unique(L_img));
                     cells=cells(2:end);                  %% Deleting cell 0 from range
                     neighbours = {};
-                    for cel = 1:max(Img_L(:))
-                        BW = bwperim(Img_L==cel);
+                    for cel = 1:max(L_img(:))
+                        BW = bwperim(L_img==cel);
                         [pi,pj]=find(BW==1);
 
                         BW_dilate = imdilate(BW,se);
                         pixels_neighs = BW_dilate==1;
-                        neighs = unique(Img_L(pixels_neighs));
+                        neighs = unique(L_img(pixels_neighs));
                         neighbours{cel} = neighs(neighs ~= 0 & neighs ~= cel);
                     end
 
@@ -127,13 +127,13 @@ function Calculate_neighbors_polygon_distribution(currentPath)
                     %-- files with a white frame (not cells at that frame) --%
                     if borderIncorrect
                         borderCells = [numIncorrectAreas, cat(1, neighbours{numIncorrectAreas})'];
-                        allCells = 1:max(Img_L(:));
+                        allCells = 1:max(L_img(:));
                         noBorderCells = ismember(allCells, borderCells);
                         noBorderCells = allCells(noBorderCells == 0);
                     else
                        %-- files with cells partial images at boundaries --%
-                        for cell=2:max(Img_L(:))
-                            if unique(Img_det_bord(Img_L == cell))~=1
+                        for cell=2:max(L_img(:))
+                            if unique(Img_det_bord(L_img == cell))~=1
                                 noBorderCells = [noBorderCells, cell];
                             else
                                 borderCells = [borderCells, cell];
@@ -143,10 +143,10 @@ function Calculate_neighbors_polygon_distribution(currentPath)
 
                     % Removing cells isolated from the valid cells
                     noValidCells = unique(borderCells);
-                    validCells = unique(noBorderCells);
+                    valid_cells = unique(noBorderCells);
                     vecinos_real = neighbours;
 
-                    celulas_validas_previa = validCells;
+                    celulas_validas_previa = valid_cells;
                     celulas_no_validas_previa = noValidCells;
                     flag = 0;
                     for j=1:length(celulas_validas_previa) % Bucle que recorre todas celulas validas para comprobar
@@ -157,22 +157,21 @@ function Calculate_neighbors_polygon_distribution(currentPath)
                             no_coincidencia(x)=isempty(find(vec_cel_ind_j(x)==celulas_no_validas_previa)); %Variable que vale 1 si no existe coincidencia de la celula j con alguna de las celulas no validas
                         end
                         if sum(no_coincidencia)==0
-                            validCells=validCells(validCells~=celulas_validas_previa(j));
+                            valid_cells=valid_cells(valid_cells~=celulas_validas_previa(j));
                             noValidCells=sort([celulas_validas_previa(j),celulas_no_validas_previa]);
                             flag=1;
                         end
                     end
                     if (flag==0)
-                        validCells=celulas_validas_previa;                                        %%VARIABLE A GUARDAR
+                        valid_cells=celulas_validas_previa;                                        %%VARIABLE A GUARDAR
                         noValidCells=celulas_no_validas_previa;
                     end
 
-                    if size(validCells, 1) == 0
+                    if size(valid_cells, 1) == 0
                         error('No valid cells!');
                     end
-                    vecinos = neighbours;
-                    celulas_validas = validCells;
-                    save(outputFile, 'vecinos', 'celulas_validas');
+                    neighs_real = neighbours;
+                    save(outputFile, 'neighs_real', 'valid_cells', 'L_img');
                 end
             end
         end
