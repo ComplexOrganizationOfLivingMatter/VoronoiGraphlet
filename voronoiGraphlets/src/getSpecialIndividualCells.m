f63a80dbcd8756dd68ebd5c7bff8374e03f00767
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-05 16:32:20 +0100

    intersecting with final valid cells

--- voronoiGraphlets/src/getSpecialIndividualCells.m
+++ voronoiGraphlets/src/getSpecialIndividualCells.m
@@ -1,9 +1,10 @@
-function [ ] = getSpecialIndividualCells( infoPath, outputPath, additionalDataPath)
+function [ ] = getSpecialIndividualCells( infoPath, outputPath, additionalDataPath, validCellsPath, removingGraphlets)
 %GETSPECIALINDIVIDUALCELLS Summary of this function goes here
 %   Detailed explanation goes here
 
     allFilesInfo = getAllFiles(infoPath);
     allFilesData = getAllFiles(additionalDataPath);
+    allFilesValidCells = getAllFiles(validCellsPath);
     pathSplitted = strsplit(additionalDataPath, '\');
     typeOfData = pathSplitted{end-1};
     for numFile = 1:size(allFilesData,1)
@@ -12,6 +13,12 @@ function [ ] = getSpecialIndividualCells( infoPath, outputPath, additionalDataPa
         fileName = fullPathFileSplitted{end};
         
         if isempty(strfind(fileName, '_data.mat')) && isempty(strfind(fileName, '.mat')) == 0 %Only analyze additional information
+            fileNameSplitted = strsplit(fileName, '_');
+            
+            cellValidsFile = cellfun(@(x) isempty(strfind(x, strcat(strjoin(fileNameSplitted(1:2), '_'), '_data.mat'))) == 0, allFilesValidCells);
+            cellValidsFileName = allFilesValidCells{cellValidsFile};
+            load(cellValidsFileName);
+            
             cells = load(fullPathFile);
             nameVars = fieldnames(cells);
             cells = struct2cell(cells);
@@ -19,9 +26,11 @@ function [ ] = getSpecialIndividualCells( infoPath, outputPath, additionalDataPa
             graphletFile = cellfun(@(x) isempty(strfind(x, strjoin(fileNameSplitted(1:2), '_'))) == 0, allFilesInfo);
             
             matrixGraphlets = dlmread(allFilesInfo{graphletFile});
+            matrixGraphlets(:, removingGraphlets) = 0;
             for i = 1:length(nameVars)
-                especialCells = cells{i};
+                especialCells = intersect(cells{i}, finalValidCells);
                 for numCell = 1:size(especialCells)
+                    
                     finalOutputFile = strcat(outputPath, strjoin(fileNameSplitted(1:2), '_'), '_', nameVars{i}, '_' ,num2str(especialCells(numCell)), '_numNeighbours_', num2str(matrixGraphlets(especialCells(numCell), 1)),'.ndump2');
                     if exist(finalOutputFile, 'file') ~= 2
                         dlmwrite(finalOutputFile, matrixGraphlets(especialCells(numCell), :), ' ');
