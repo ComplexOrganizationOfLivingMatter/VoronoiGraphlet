3076d293ae4cf6ffcfd9d18f19942825a46fba97
Pablo Vicente Munuera <pablovm1990@gmail.com>
2016-12-09 13:16:53 +0100

    rosetta files

--- voronoiGraphlets/src/getValidCellsFromROI.m
+++ voronoiGraphlets/src/getValidCellsFromROI.m
@@ -27,9 +27,56 @@ function [ finalValidCells ] = getValidCellsFromROI(currentPath, maxPathLength, 
         end
         %Check which files we want.
         if isempty(strfind(lower(diagramName), '_data.mat')) == 0 && exist(outputFile, 'file') ~= 2 && isempty(strfind(lower(strjoin(diagramNameSplitted, '\')), 'columns'))
-            if isempty(strfind(lower(diagramName), 'neo'))
+            clear vecinos Vecinos celulas_validas
+            if isempty(strfind(outputFile, 'neo')) == 0 %NEO files
+                load(fullPathFile);
+                if maxPathLength == 4
+                    finalValidCells = valid_cells_4;
+                elseif maxPathLength == 5
+                    finalValidCells = valid_cells_5;
+                end
+                celulas_validas = valid_cells;
+                vecinos = neighs_real;
+                save(outputFile, 'finalValidCells', 'celulas_validas', 'vecinos');
+            elseif isempty(strfind(outputFile, 'rosetta')) == 0 %rosetta
+                load(fullPathFile);
+                if maxPathLength == 4
+                    finalTargetCells = valid_neighbors_ring_4;
+                    finalValidCells_temp = valid_cells_4;
+                elseif maxPathLength == 5
+                    finalTargetCells = valid_neighbors_ring_5;
+                    finalValidCells_temp = valid_cells_5;
+                end
+                
+                %finalValidCells = filteredFinalValidCells(finalValidCells_temp, neighs_real, maxPathLength, fullPathFile, finalTargetCells, 'rosetta' );
+                adj_matrix=zeros(size(neighs_real,2));
+
+                for i=1:size(neighs_real,2) 
+                    neighs=neighs_real{1,i}; 
+                    adj_matrix(i,neighs)=1;
+                end
+
+                %Distance matrix since adjacency matrix
+                [dist_matrix] = graphallshortestpaths(sparse(adj_matrix));
+
+                dist_valid_to_no_valid=dist_matrix(valid_cells, finalTargetCells);
+
+                closest_no_valid_cell=min(dist_valid_to_no_valid');
+                
+                finalValidCells=valid_cells(closest_no_valid_cell<=maxPathLength);
+
+                celulas_validas = valid_cells;
+                vecinos = neighs_real;
+                
+                %final valid cells
+                numNeighbours = cellfun(@(x) length(x), neighs_real(finalValidCells));
+                polygonDistributionFinalValidCells = [sum(numNeighbours == 3), sum(numNeighbours == 4), sum(numNeighbours == 5), sum(numNeighbours == 6), sum(numNeighbours == 7), sum(numNeighbours == 8), sum(numNeighbours == 9), sum(numNeighbours == 10)] / length(numNeighbours); 
+                %All valid cells
+                numNeighbours = cellfun(@(x) length(x), neighs_real(valid_cells));
+                polygonDistributionValidCells = [sum(numNeighbours == 3), sum(numNeighbours == 4), sum(numNeighbours == 5), sum(numNeighbours == 6), sum(numNeighbours == 7), sum(numNeighbours == 8), sum(numNeighbours == 9), sum(numNeighbours == 10)] / length(numNeighbours);
+                save(outputFile, 'finalValidCells', 'celulas_validas', 'vecinos', 'finalTargetCells', 'polygonDistributionFinalValidCells', 'polygonDistributionValidCells');
+            else
                 fullPathFile
-                clear vecinos Vecinos celulas_validas
                 load(fullPathFile);%load valid cells and neighbours
 
                 if exist('vecinos', 'var') == 1
@@ -88,16 +135,6 @@ function [ finalValidCells ] = getValidCellsFromROI(currentPath, maxPathLength, 
                 celulas_validas = validCells;
                 save(outputFile, 'finalValidCells', 'celulas_validas', 'vecinos');
                 clear vecinos celulas_validas
-            else %NEO files
-                load(fullPathFile);
-                if maxPathLength == 4
-                    finalValidCells = valid_cells_4;
-                elseif maxPathLength == 5
-                    finalValidCells = valid_cells_5;
-                end
-                celulas_validas = valid_cells;
-                vecinos = neighs_real;
-                save(outputFile, 'finalValidCells', 'celulas_validas', 'vecinos');
             end
         end
     end
