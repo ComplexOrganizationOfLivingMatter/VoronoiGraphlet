7e4b690e6b3a0e0667be6ea4c15461fe1fbbbf36
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-02-17 17:58:44 +0100

    more info

--- voronoiGraphlets/main.m
+++ voronoiGraphlets/main.m
@@ -19,6 +19,8 @@ pipelineGraphletsVoronoi('voronoiNoise\');
 pipelineGraphletsVoronoi('voronoiWeighted\');
 pipelineGraphletsVoronoi('biologicalImagesAndVoronoi\');
 pipelineGraphletsVoronoi('Neo\');
+pipelineGraphletsVoronoi('rosetta\');
+pipelineGraphletsVoronoi('voronoiEmbo\');
 
 %% 8 initial graphics
 getPercentageOfHexagons('results\graphletResultsFiltered\allOriginal\', '', 'maxLength5');
@@ -32,6 +34,7 @@ analyzeGraphletDistances(strcat('results\distanceMatrix\SickEpitheliums\', 'Basi
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'BasicaParcial\NeighboursOfCancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'BasicaParcial\CancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\Neo\', 'BasicaParcial\'), 'gdda');
+analyzeGraphletDistances(strcat('results\distanceMatrix\rosetta\', 'BasicaParcial\'), 'gdda');
 
 
 %Basica
@@ -41,6 +44,7 @@ analyzeGraphletDistances(strcat('results\distanceMatrix\SickEpitheliums\', 'Basi
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'Basica\NeighboursOfCancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'Basica\CancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\Neo\', 'Basica\'), 'gdda');
+analyzeGraphletDistances(strcat('results\distanceMatrix\rosetta\', 'Basica\'), 'gdda');
 
 %Total Parcial
 analyzeGraphletDistances(strcat('results\distanceMatrix\biologicalImagesAndVoronoi\', 'TotalParcial\'), 'gdda');
@@ -49,6 +53,7 @@ analyzeGraphletDistances(strcat('results\distanceMatrix\SickEpitheliums\', 'Tota
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'TotalParcial\NeighboursOfCancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'TotalParcial\CancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\Neo\', 'TotalParcial\'), 'gdda');
+analyzeGraphletDistances(strcat('results\distanceMatrix\rosetta\', 'TotalParcial\'), 'gdda');
 
 
 %Total
@@ -58,6 +63,7 @@ analyzeGraphletDistances(strcat('results\distanceMatrix\SickEpitheliums\', 'Tota
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'Total\NeighboursOfCancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\voronoiWeighted\', 'Total\CancerCells\'), 'gdda');
 analyzeGraphletDistances(strcat('results\distanceMatrix\Neo\', 'Total\'), 'gdda');
+analyzeGraphletDistances(strcat('results\distanceMatrix\rosetta\', 'Total\'), 'gdda');
 
 %Total
 %analyzeGraphletDistances(strcat('results\comparisons\Total\', 'maxLength5\'), 'gdda');
@@ -94,3 +100,50 @@ for i = 1:length(names)
         plot(differenceMean(i), distanceMatrix(i), 'o', 'color', [0.0 0.6 0.0]);
     end
 end
+
+for i = 1:3
+    splitFileInNeighbours(strcat('E:\Pablo\VoronoiGraphlet\voronoiGraphlets\test\eye\omm_', num2str(i),'.ndump2'));
+end
+
+% getValidCellsFromROI('data\rosetta\data\data_images_disk_5\', 5, 'test\');
+% getValidCellsFromROI('data\rosetta\data\data_images_disk_6\', 5, 'test\');
+% getValidCellsFromROI('data\rosetta\data\data_images_disk_7\', 5, 'test\');
+
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 4, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 5, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 6, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 7, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 8, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 9, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\TotalParcial\' , 10, 'gdda' )
+
+
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 4, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 5, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 6, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 7, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 8, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 9, 'gdda' )
+compareIndividualCells( 'results\graphletResultsIndividual\biologicalImagesAndVoronoi\Total\' , 10, 'gdda' )
+
+%Compare 3D
+compare3D( 'results\comparisons\EveryFile\Total\AgainstHexagons\');
+
+
+%% Info persistence homology
+
+infoPersistenceHomology('data\biologicalImagesAndVoronoi\Eyes\images\');
+infoPersistenceHomology('data\biologicalImagesAndVoronoi\dWP\images\');
+infoPersistenceHomology('data\biologicalImagesAndVoronoi\dWL\images\');
+infoPersistenceHomology('data\biologicalImagesAndVoronoi\cNT\images\');
+infoPersistenceHomology('data\voronoiNoise\images\');
+
+%% CVT embo
+fullPathGraphlet = 'D:\Pablo\VoronoiGraphlet\voronoiGraphlets\results\graphletResultsFiltered\voronoiEmbo\TotalPartialV1_V5\gdda.txt';
+distanceMatrixInit = dlmread(fullPathGraphlet, '\t', 1, 1);
+names = importfileNames(fullPathGraphlet);
+[names, it] = sort(names);
+distanceMatrix = distanceMatrixInit(it, it);
+idsVoronoi = cellfun(@(x) isempty(strfind(x, 'Diagrama_')) == 0, names);
+distanceFinal = distanceMatrix(idsVoronoi, idsVoronoi == 0);
+mean(distanceFinal(:))
