3c38ce57243f6620c86ca7695f9b3ed92f171580
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-10-10 15:17:35 +0200

    bugfix and preparing for refactor

--- voronoiGraphlets/src/comparePercentageOfHexagonsAgainstComparisonWithRegularHexagons.m
+++ voronoiGraphlets/src/comparePercentageOfHexagonsAgainstComparisonWithRegularHexagons.m
@@ -11,7 +11,7 @@ function [ ] = comparePercentageOfHexagonsAgainstComparisonWithRegularHexagons( 
 %     load(strcat(totalGraphletsPath, 'allDifferences.mat'))
     
     %%GDDRV vs GDDRH
-    if isequal(kindOfGraphics, 'GDDRV_GDDRH')
+    if isequal(kindOfGraphics, 'GDDRV_GDDH')
         load(strrep(strcat(currentPath, 'allDifferences.mat'), 'AgainstVoronoi1', 'AgainstHexagons'))
     elseif isequal(kindOfGraphics, 'GDDV5_GDDH')
         load(strrep(strcat(currentPath, 'allDifferences.mat'), 'AgainstVoronoi5', 'AgainstHexagons'))
@@ -130,6 +130,34 @@ function [ ] = comparePercentageOfHexagonsAgainstComparisonWithRegularHexagons( 
         error('Wrong percentages');
     end
 
+    
+    h1 = figure('units','normalized','outerposition',[0 0 1 1], 'Color', [1 1 1]);
+%     classes = {'BC' 'BCA' [0 0.200000000000000 0];
+%     'omm' 'Eye' [1 0.400000000000000 0];
+%     'cNT' 'cNT' [0.200000000000000 0.800000000000000 1];
+%     'dWL' 'dWL' [0 0.600000000000000 0];
+%     'dWP' 'dWP' [1 0 0];
+%     'imagen' 'CVT' [0.800000000000000 0.800000000000000 0.800000000000000];
+%     'disk' 'CVTw1' [0.200000000000000 0.600000000000000 0.600000000000000];
+%     'Case-II' 'Case II' [0 0 0.600000000000000];
+%     'Case-III' 'Case III' [1 0.800000000000000 1];
+%     'Case-IV' 'Case IV' [1 0.200000000000000 1];
+%     'dMWP' 'dMWP' [0.800000000000000 0 0.600000000000000];
+%     'Atrophy-Sim' 'Atrophy' [0.600000000000000 0 1];
+%     'Control-Sim-Prol' 'Control' [0.200000000000000 0.800000000000000 1];
+%     'Control-Sim-no-Prol' 'Control No Prol' [1 1 0];
+%     'BNA' 'BNA' [0 0 0];
+%     'disk' 'CVTw2' [0.200000000000000 0.400000000000000 0.600000000000000];
+%     'neo' 'Neo' [0.200000000000000 0.400000000000000 0.600000000000000];
+%     'rosetta' 'Rosetta' [0.400000000000000 0 0];
+%     'voronoiNoise' 'CVTn' [1 0 0.315789473684211]};
+%     
+%     indicesSelected = [7, 16, 19];
+%     
+%     meanIndices = cellfun(@(x) isempty(strfind(x, 'totalGraphlets')) == 0, names);
+%     [~] = plottingInfo(currentPath, h1, differenceWithRegularHexagon(meanIndices == 0, :), percentageOfHexagons(:, meanIndices == 0), [], names(meanIndices == 0), classes(indicesSelected, :), 0);
+%     [figureValues] = plottingInfo(currentPath, h1, differenceWithRegularHexagon(meanIndices, :), percentageOfHexagons(:, meanIndices), [], names(meanIndices), classes(indicesSelected, :), 1);
+%     
     numberOfTypes = 19;
     colors = hsv(numberOfTypes);
     colors(1, :) = [0.0 0.2 0.0]; %BCA
