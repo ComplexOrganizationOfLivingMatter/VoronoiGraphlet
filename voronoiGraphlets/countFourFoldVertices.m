980b5fd0a5591b7afc70b030a8d58575a6ff380d
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-14 16:30:00 +0200

    for the two kind of images

--- voronoiGraphlets/countFourFoldVertices.m
+++ voronoiGraphlets/countFourFoldVertices.m
@@ -1,10 +1,18 @@
 
 inputDir = 'D:\Pablo\VoronoiGraphlet\voronoiGraphlets\data\RobImages\MbsRNAi\FinalSegmentation\*.tif';
-imgFiles = dir(inputDir);
+imgFilesInit = dir(inputDir);
+inputDir = 'D:\Pablo\VoronoiGraphlet\voronoiGraphlets\data\Neo\FinalUsed\*.tif';
+imgFiles2 = dir(inputDir);
+
+imgFiles = [imgFilesInit; imgFiles2];
 
 for numFile = 1:length(imgFiles)
     img = imread(fullfile(imgFiles(numFile).folder, imgFiles(numFile).name));
-    L_img = bwlabel(img == 0, 4);
+    if size(imgFilesInit, 1) >= numFile
+        L_img = bwlabel(img == 0, 4);
+    else
+        L_img = bwlabel(img == 255);
+    end
     imgNeigbhours = calculate_neighbours(L_img);
     imgFiles(numFile).percentageOfFourFoldVertices = getFourFoldVertices(imgNeigbhours,L_img);
 end
